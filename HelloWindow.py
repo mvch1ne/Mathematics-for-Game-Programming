ed23ac8eba2ce44620cc6d2a4d3efeb61351cd92
mvch1ne <sabithadidariff@gmail.com>
2025-09-04 08:23:37 +0000

    Working

--- HelloWindow.py
+++ HelloWindow.py
@@ -4,3 +4,10 @@ screen_width = 1000
 screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        pygame.display.update()
+    pygame.quit()
